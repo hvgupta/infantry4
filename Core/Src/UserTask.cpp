92968ebf38ed6f5940895d3d57647e0e7a8fd337
coopjz <junzhewang_2019@163.com>
2023-04-13 21:09:38 +0800

    :swan: ed jjw

--- Core/Src/UserTask.cpp
+++ Core/Src/UserTask.cpp
@@ -8,21 +8,24 @@
  * @copyright Copyright (c) 2022
  */
 #include <cstdint>
+
 #include "FreeRTOS.h"
+#include "RefereeClientUIManager.hpp"
+#include "RefereeSystemTask.hpp"
+#include "RosComm.hpp"
 #include "gpio.h"
 #include "main.h"
-#include "RosComm.hpp"
 #include "task.h"
 #include "tim.h"
 #include "usart.h"
-#include "DJIMotor.hpp"
 
+// #include "DJIMotor.hpp"
 
 StackType_t uxBlinkTaskStack[configMINIMAL_STACK_SIZE];
 StaticTask_t xBlinkTaskTCB;
 
 // #define HZ2PSC(x) (144000000/5000/(x))
-inline uint16_t HZ2PSC(float freq) { return (int16_t)(144000000.0f/5000.0f/freq); }
+inline uint16_t HZ2PSC(float freq) { return (int16_t)(144000000.0f / 5000.0f / freq); }
 
 void buzzerOn(uint16_t psc, uint16_t comp)
 {
@@ -50,13 +53,13 @@ void blink(void *pvPara)
     }
 }
 
-void processGimbal(uint8_t* data, uint16_t len)
+void processGimbal(uint8_t *data, uint16_t len)
 {
     static volatile int here = 0;
     here += 1;
 }
 
-void processShooter(uint8_t* data, uint16_t len)
+void processShooter(uint8_t *data, uint16_t len)
 {
     static volatile int here = 0;
     here += 1;
@@ -65,45 +68,9 @@ StackType_t comTestStack[configMINIMAL_STACK_SIZE];
 StaticTask_t comTestTCB;
 void com(void *)
 {
-    using namespace Core::Communication;
-    static RosComm::ChassisStatus chassisStatus;
-    static RosComm::FrameHeader chassisHeader;
-    chassisHeader.dataLen = sizeof(RosComm::ChassisStatus);
-    chassisHeader.protocolID = RosComm::CHASSIS_MSG;
-    chassisStatus.wVel = 114.0;
-    chassisStatus.xVel = 514.0;
-    chassisStatus.yVel = 618.3;
-
-    static RosComm::GimbalStatus gimbalStatus;
-    static RosComm::FrameHeader gimbalHeader;
-    gimbalStatus.q[2] = 3;
-    gimbalHeader.dataLen = sizeof(RosComm::GimbalStatus);
-    gimbalHeader.protocolID = RosComm::GIMBAL_MSG;
-
-    static RosComm::CVMode cvMode;
-    static RosComm::FrameHeader cvHeader;
-    cvHeader.dataLen = sizeof(RosComm::CVMode);
-    cvHeader.protocolID = RosComm::CVMODE_MSG;
-
-    cvMode.color = RosComm::CVMode::RED;
-    cvMode.curCVMode = Core::Communication::RosComm::CVMode::MANULA;
-    cvMode.bulletSpeed = 12.34f;
-
-    registerProcessFunction(RosComm::GIMBAL_CMD, processGimbal);
-    registerProcessFunction(RosComm::SHOOTER_CMD, processShooter);
-
-    DJIMotor::DJIMotor& motor = DJIMotor::getMotor(0x201);
-    motor.setOutputCurrent(-10000);
-    while(1)
-    {
-        RosComm::transmit(chassisHeader, reinterpret_cast<uint8_t*>(&chassisStatus));
-        RosComm::transmit(gimbalHeader, reinterpret_cast<uint8_t*>(&gimbalStatus));
-        RosComm::transmit(cvHeader, reinterpret_cast<uint8_t*>(&cvMode));
-        DJIMotor::sendMotorGroup(0);
-        vTaskDelay(1);
-    }
+    while (1)
+        ;
 }
-
 
 /**
  * @brief Create user tasks
@@ -111,8 +78,9 @@ void com(void *)
 void startUserTasks()
 {
     using namespace Core::Communication;
-    RosComm::init();
-    // DJIMotor::init();
+    // RosComm::init();
+    RefereeSystemUI::initUI();
+    xTaskCreateStatic(refereeTask, "referee communication", 4096, NULL, 8, uxRefereeTaskStack, &xRefereeTaskTCB);
     xTaskCreateStatic(blink, "blink", configMINIMAL_STACK_SIZE, NULL, 0, uxBlinkTaskStack, &xBlinkTaskTCB);
     xTaskCreateStatic(com, "com", configMINIMAL_STACK_SIZE, NULL, 0, comTestStack, &comTestTCB);
 }
